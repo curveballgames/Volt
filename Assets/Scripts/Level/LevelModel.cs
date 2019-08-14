d80074a6c37115f68dc550a4595a46160172fefa
Chris Waldron <chriswaldron15@gmail.com>
2019-08-14 19:50:57 +0100

    Cash management

--- Assets/Scripts/Level/LevelModel.cs
+++ Assets/Scripts/Level/LevelModel.cs
@@ -8,14 +8,16 @@
         public BuildingIdentifier[] AvailableBuildings;
         public ChallengeModel[] Challenges;
         public LevelCondition[] Conditions;
+        public int StartingFunds;
 
-        public LevelModel(string levelName, string sceneName, BuildingIdentifier[] availableBuildings, ChallengeModel[] challenges, LevelCondition[] conditions)
+        public LevelModel(string levelName, string sceneName, BuildingIdentifier[] availableBuildings, ChallengeModel[] challenges, LevelCondition[] conditions, int startingResources)
         {
             LevelName = levelName;
             SceneName = sceneName;
             AvailableBuildings = availableBuildings;
             Challenges = challenges;
             Conditions = conditions;
+            StartingFunds = startingResources;
         }
     }
 }
---
+++ Assets/Scripts/Player/FundManager.cs
@@ -0,0 +1,37 @@
+﻿using Curveball;
+using UnityEngine;
+
+namespace Volt
+{
+    public class FundManager : CBGGameObject
+    {
+        public static int Funds { get; private set; }
+
+        private void Awake()
+        {
+            Funds = LevelStore.CurrentLevel.StartingFunds;
+        }
+
+        public static void AddFunds(int funds)
+        {
+            if (funds <= 0)
+            {
+                Debug.LogWarning("Tried to add non-positive funds: " + funds);
+                return;
+            }
+
+            Funds += funds;
+        }
+
+        public static void RemoveFunds(int funds)
+        {
+            if (funds <= 0)
+            {
+                Debug.LogWarning("Tried to remove non-positive funds: " + funds);
+                return;
+            }
+
+            Funds -= funds;
+        }
+    }
+}
